b397a9c042c4aa70b3fc09bcff061143688d10bd
Wonders Victor <wondersprince1@gmail.com>
2024-05-06 12:29:52 +0000

    Syncing Async

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Task 3: Asyncio Tasks'''
+import asyncio
+from typing import Awaitable
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+def task_wait_random(max_delay: int) -> Awaitable[float]:
+    '''
+    Takes an integer max_delay and returns a asyncio.Task.
+    '''
+    return asyncio.create_task(wait_random(max_delay))
