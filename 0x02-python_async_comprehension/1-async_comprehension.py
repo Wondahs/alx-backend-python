2de81f80b7ca4a789ba74f973f733fb4b89e70a1
Wonders Victor <wondersprince1@gmail.com>
2024-05-07 12:38:10 +0000

    More annotation fixes

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,7 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''Async Generator'''
-    result = []
+    result: List[float] = []
     async for i in async_generator():
         result.append(i)
     return result
